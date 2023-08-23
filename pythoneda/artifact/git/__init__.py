1809d6739e17f4374f257efced463e73205727b9
rydnr <github@acm-sl.org>
2023-08-23 21:11:17 +0200

    __init__.py changes

--- pythoneda/artifact/git/__init__.py
+++ pythoneda/artifact/git/__init__.py
@@ -19,3 +19,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+try:
+    from pythoneda.artifact.git.git_artifact import GitArtifact
+except ImportError:
+    pass
